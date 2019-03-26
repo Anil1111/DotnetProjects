f1c4710eb65fd41d74482cffe4fb77d84cb0ae0e
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-26 18:05:26 -0400

    updated ErrorLoggerTest with tests that test for an Raised Event

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -22,9 +23,19 @@ namespace TestNinja.UnitTests
         public void Log_InvalidError_ThrowArgumentNullException(string error)
         {
             var logger = new ErrorLogger();
-            //logger.Log(error);
-
+           
             Assert.That(()=> logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args;};
+            logger.Log("a");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+
         }
 
     }
