8e3744bdfb58098adeadb4cef72eb01ee5c2b16a
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-25 20:38:39 -0400

    Added tests for htmlformatter

--- LangFeatures/LangFeatures/Models/Product.cs
+++ LangFeatures/LangFeatures/Models/Product.cs
@@ -2,19 +2,29 @@
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
         public string Name { get; set; }
+        public string Category { get; set; } = "Water Sports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; }
+
+       
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
 
-            Product lifeJacket = new Product
+            Product lifeJacket = new Product(false)
             {
                 Name = "Lifejacket",
                 Price = 48.95M
---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formmatter = new HtmlFormatter();
+            var result = formmatter.FormatAsBold("abc");
+
+            //specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //more general
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+
+
+
+
+        }
+
+    }
+}
