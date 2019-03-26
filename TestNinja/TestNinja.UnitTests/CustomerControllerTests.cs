5d24b22bd2d88abdcb497007506d1a057cb788ff
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-25 21:26:12 -0400

    Added CustomerControllerTests which test return types

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            //NotFound type
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //NotFound type or one of it's derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IsIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+
+    }
+}
