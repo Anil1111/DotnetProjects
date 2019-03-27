96227cb9ae83dc356f103209a9e8c001b08f5e83
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-26 21:37:12 -0400

    Added FizzBuzzTests

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void FizzBuzz_DivisibleBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void FizzBuzz_DivisibleBy3_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(6);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void FizzBuzz_DivisibleBy5_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(10);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+    }
+}
+
