ab30d931b7ef4aabd2883b7e5d27c1e0ccf4656b
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-26 22:49:36 -0400

    updated demerit file

--- TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -12,17 +13,22 @@ namespace TestNinja.UnitTests
         public void CalculateDemeritPoints_SpeedIsLessThan0OrGreaterThanMaxSpeed_ReturnsArgOutOfRangeExp(int speed)
         {
             var dpc = new DemeritPointsCalculator();
-            Assert.That(() => dpc.CalculateDemeritPoints(speed), Throws.Exception);
+            Assert.That(() => dpc.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
         }
 
         [Test]
-        [TestCase(65)]
-        [TestCase(1)]
-        public void CalculateDemeritPoints_SpeedIsLessThanOrEqualToZero_ReturnZero(int speed)
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_SpeedIsLessThanOrEqualToZero_ReturnZero(int speed, int expectedResult)
         {
-            var dpc = new DemeritPointsCalculator();
-            var result = dpc.CalculateDemeritPoints(speed);
-            Assert.That(result, Is.EqualTo(0));
+            var calculator = new DemeritPointsCalculator();
+            var points = calculator.CalculateDemeritPoints(speed);
+            Assert.That(points, Is.EqualTo(expectedResult));
+
         }
 
         [Test]
@@ -32,8 +38,6 @@ namespace TestNinja.UnitTests
             var result = dpc.CalculateDemeritPoints(80);
             Assert.That(result, Is.EqualTo(3));
         }
-
-
 
     }
 }
