be1ecaf938d0b126dd6334512bb0360a056f9f46
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-26 22:32:44 -0400

    Added DemeritPointsCalculatorTests

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsLessThan0OrGreaterThanMaxSpeed_ReturnsArgOutOfRangeExp(int speed)
+        {
+            var dpc = new DemeritPointsCalculator();
+            Assert.That(()=>dpc.CalculateDemeritPoints(speed),Throws.Exception);
+        }
+
+        [Test]
+        [TestCase(65)]
+        [TestCase(1)]
+        public void CalculateDemeritPoints_SpeedIsLessThanOrEqualToZero_ReturnZero(int speed)
+        {
+            var dpc = new DemeritPointsCalculator();
+            var result = dpc.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_OnePointFor5KMOverSpeedLimit_ReturnDemeritPoints()
+        {
+            var dpc = new DemeritPointsCalculator();
+            var result = dpc.CalculateDemeritPoints(80);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+
+
+    }
+}
--- TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -7,25 +7,27 @@ namespace TestNinja.UnitTests
     public class FizzBuzzTests
     {
         [Test]
-        public void FizzBuzz_DivisibleBy3And5_ReturnsFizzBuzz()
+        public void GetOutput_DivisibleBy3And5_ReturnsFizzBuzz()
         {
             var result = FizzBuzz.GetOutput(15);
             Assert.That(result, Is.EqualTo("FizzBuzz"));
         }
 
         [Test]
-        public void FizzBuzz_DivisibleBy3_ReturnsFizz()
+        public void GetOutput_DivisibleBy3_ReturnsFizz()
         {
             var result = FizzBuzz.GetOutput(6);
             Assert.That(result, Is.EqualTo("Fizz"));
         }
 
         [Test]
-        public void FizzBuzz_DivisibleBy5_ReturnBuzz()
+        public void GetOutput_DivisibleBy5_ReturnBuzz()
         {
             var result = FizzBuzz.GetOutput(10);
             Assert.That(result, Is.EqualTo("Buzz"));
         }
+
     }
 }
+
 
