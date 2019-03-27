03171b28d59f30d6ab1c29cd1dec71021b9d4fea
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-27 18:23:07 -0400

    initial commit for Razor project

--- LangFeatures/LangFeatures/Models/MyExtensionMethods.cs
+++ LangFeatures/LangFeatures/Models/MyExtensionMethods.cs
@@ -1,11 +1,14 @@
-﻿namespace LangFeatures.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LangFeatures.Models
 {
     public static class MyExtensionMethods
     {
-        public static decimal TotalPrices(this ShoppingCart cartParam)
+        public static decimal TotalPrices(this IEnumerable<Product>products)
         {
             decimal total = 0;
-            foreach (Product prod in cartParam.Products)
+            foreach (Product prod in products)
             {
                 total += prod?.Price ?? 0;
             }
