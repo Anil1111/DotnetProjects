a62553b239e4a67a0b3f14be2ee8731c1ce9a473
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-27 21:47:21 -0400

    added StackTests

---
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_WhenObjectIsNull_ReturnsArgumentNullException()
+        {
+            var stack = new Fundamentals.Stack<object>();
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ObjectAddedToList_ListIsEqualToOne()
+        {
+            var stack = new Fundamentals.Stack<object>();
+            stack.Push("string");
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_CountIsZero_ReturnsInvalidOperationException()
+        {
+            var stack = new Fundamentals.Stack<object>();
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_RemoveItemFromList_CountIsOneLess()
+        {
+            var stack = new Fundamentals.Stack<object>();
+            for (var i = 0; i < 5; i++)
+            {
+                stack.Push(new object());
+            }
+
+            stack.Pop();
+            var result = stack.Count;
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Peek_CountIsZero_ReturnsInvalidOperationException()
+        {
+            var stack = new Fundamentals.Stack<object>();
+            Assert.That(()=>stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_Peeking_ReturnsTopStackValue()
+        {
+            var stack = new Fundamentals.Stack<object>();
+            stack.Push(1);
+            stack.Push(2);
+            var result = stack.Peek();
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+
+
+    }
+}
