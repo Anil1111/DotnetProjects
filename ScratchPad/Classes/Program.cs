46ed7103fc166bf6f7c92b983b62243594c9812c
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-01 20:41:16 -0400

    Added ScratchPad

---
+++ ScratchPad/Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {Name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            var obj = new Person {Name = str};
+            return obj;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var person = Person.Parse("Anthony");
+            person.Introduce("Rick");
+        }
+    }
+}
