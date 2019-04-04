88ec95c60b3594d568659ae07a284d8c53b33fce
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-03 20:10:12 -0400

    added stack class

---
+++ ScratchPad/Inheritance/Classes/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Inheritance.Classes
+{
+    public class Stack
+    {
+        private readonly ArrayList _list = new ArrayList();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Objects cannot be null");
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+            var objToDelete = _list[_list.Count - 1];
+            _list.RemoveAt(_list.Count - 1);
+            return objToDelete;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
--- ScratchPad/Inheritance/Program.cs
+++ ScratchPad/Inheritance/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using Inheritance.Classes;
 
 namespace Inheritance
@@ -7,14 +10,34 @@ namespace Inheritance
     {
         private static void Main(string[] args)
         {
+            var stack = new Inheritance.Classes.Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+
+        private static void UpCastTwo()
+        {
+            //StreamReader reader = new StreamReader(new MemoryStream());
+            ArrayList list = new ArrayList();
+            list.Add(1);
+
+            var anotherList = new List<int>();
+            anotherList.Add(1);
+        }
+
+        private static void UpCastingExmpOne()
+        {
             Text text = new Text();
-            Shape shape = text;         //no up-casting required
+            Shape shape = text;                 //no up-casting required
 
             text.Width = 200;
             shape.Width = 100;
-            Console.WriteLine(text.Width); //both text and shape are ref'd to same objects (just different views)
-
-
+            Console.WriteLine(text.Width);      //both text and shape are ref'd to same objects (just different views) Ans: 100
         }
     }
 }
