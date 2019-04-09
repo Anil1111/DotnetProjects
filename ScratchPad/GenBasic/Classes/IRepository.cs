63c5216b146d0a586dfe829298788f00250bd1b7
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-09 11:14:05 -0400

    added InterfaceRepositoryTests

---
+++ ScratchPad/GenBasic/Classes/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GenBasic.Classes
+{
+    public interface IRepository <T>
+    {
+        void Add(T toAdd);
+        void Delete(T toDelete);
+        IEnumerable<T> All();
+        T Get(int index);
+    }
+}
