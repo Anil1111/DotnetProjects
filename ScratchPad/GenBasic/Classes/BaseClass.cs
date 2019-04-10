5a986c405498f8bb4ff02bffb9f75d73d7620b85
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-09 20:46:48 -0400

    added delegatesandevents

---
+++ ScratchPad/GenBasic/Classes/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿namespace GenBasic.Classes
+{
+    public abstract class BaseClass<T> { }
+
+    public class NonGenericChild : BaseClass<int> { } //non generic inheriting from a generic class
+
+    public class GenericChild<T> : BaseClass<T> { } //generic child passes on to base
+
+    public class GenericChildWithAdditionalType<TAdditional> : BaseClass<string> { } 
+
+    //more complex
+    public class ComplexBaseClass<TDiagram, TSchema> { }
+    public class OneTypeSpecified<TSchema> : ComplexBaseClass<WireDiagram, TSchema> { }
+    public class BothTypesSpecified : ComplexBaseClass<WireDiagram, NormalSchema> { }
+
+
+
+    //------------------------------------------
+    public class WireDiagram { }
+    public class NormalSchema { }
+
+}
