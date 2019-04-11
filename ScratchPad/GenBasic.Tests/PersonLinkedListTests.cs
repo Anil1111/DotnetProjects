5e87846570639c5a4f36ece2d8417f75d7af9d49
Anthony Hollis <4053071+FocusedBit@users.noreply.github.com>
2019-04-11 09:02:13 -0400

    added personlinkedlist

---
+++ ScratchPad/GenBasic.Tests/PersonLinkedListTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Headers;
+using LinkedLists.Classes;
+using Xunit;
+
+namespace Generics.Tests
+{
+    public class PersonLinkedListTests
+    {
+        [Fact]
+        public void TestConstruction_ShouldBeNull()
+        {
+            var list = new PersonLinkedList();
+            Assert.Equal(0, list.Count);
+            Assert.Null(list.Get(0));
+        }
+
+        [Fact]
+        public void Insert_AddOneMember_ReturnsCorrectData()
+        {
+            var list = new PersonLinkedList();
+            list.Insert(new Person { FirstName = "Anthony", LastName = "Hollis" });
+            Assert.Equal(1, list.Count);
+
+            var person = list.Get(0);
+            Assert.NotNull(person);
+
+            Assert.Equal("Anthony", person.FirstName);
+            Assert.Equal("Hollis", person.LastName);
+
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsTheCorrectNode()
+        {
+            var list = new PersonLinkedList();
+            list.Insert(new Person { FirstName = "Chris", LastName = "Jones" });
+            list.Insert(new Person { FirstName = "Paul", LastName = "Davies" });
+            list.Insert(new Person { FirstName = "Laura", LastName = "Colburn" });
+
+            Assert.Equal(3, list.Count);
+
+            Person person = list.Get(0);
+            Assert.NotNull(person);
+            Assert.Equal("Laura", person.FirstName);
+            Assert.Equal("Colburn", person.LastName);
+
+            person = list.Get(2);
+            Assert.NotNull(person);
+            Assert.Equal("Chris", person.FirstName);
+            Assert.Equal("Jones", person.LastName);
+
+            person = list.Get(1);
+            Assert.NotNull(person);
+            Assert.Equal("Paul", person.FirstName);
+            Assert.Equal("Davies", person.LastName);
+        }
+    }
+}
---
+++ ScratchPad/LinkedLists/Classes/PersonLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading;
+
+namespace LinkedLists.Classes
+{
+    //Non Generic LinkedList
+    public class PersonLinkedList
+    {
+        private PersonNode _head;
+        public int Count { get; set; }
+
+        public PersonLinkedList()
+        {
+            Count = 0;
+            _head = null;
+        }
+
+
+        public void Insert(Person data)
+        {
+            var node = CreateNode(data);
+            node.Next = _head;
+            _head = node;
+        }
+        private PersonNode CreateNode(Person data)
+        {
+            Count++;
+            return new PersonNode {Data = data};
+        }
+
+        public Person Get(int index)
+        {
+            var position = 0;
+            PersonNode node = _head;
+
+            while (position < index && node != null)
+            {
+                node = node.Next;
+                position++;
+            }
+
+            if (node != null)
+            {
+                return node.Data;
+            }
+
+            return null;
+
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class PersonNode
+    {
+        public Person Data;
+        public PersonNode Next;
+    }
+}
