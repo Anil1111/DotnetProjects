8936cbceb40899bb65bfecf9d02a9cf35663f9c0
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-11 21:34:37 -0400

    added linkedlisttests

---
+++ ScratchPad/GenBasic.Tests/LinkedListTests.cs
@@ -0,0 +1,44 @@
+﻿using LinkedLists.Classes;
+using Xunit;
+using Person = Generics.Tests.Utility.Classes.Person;
+
+namespace Generics.Tests
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void Get_WhenInstantiatingNewList_ShouldBeNull()
+        {
+            LinkedList<Person> list = new LinkedList<Person>();
+            Assert.Equal(0, list.Count);
+            Assert.Null(list.Get(0));
+        }
+
+        [Fact]
+        public void Get_WhenAddingOneElement_ShouldReturnCorrectValues()
+        {
+            LinkedList<Person> list = new LinkedList<Person>();
+            list.Insert(new Person{FirstName = "Anthony", LastName = "Hollis"});
+            Assert.NotNull(list.Get(0));
+            Assert.Equal("Anthony", list.Get(0).FirstName );
+            Assert.Equal("Hollis", list.Get(0).LastName);
+        }
+
+        [Fact]
+        public void Get_InstantiateWithInt_ShouldBeNull()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            Assert.Equal(0, list.Count);
+            Assert.Equal(0, list.Get(0));
+        }
+
+        [Fact]
+        public void Get_WhenAddingOneInt_ShouldReturnCorrectValues()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.Insert(42);
+            Assert.Equal(1, list.Count);
+            Assert.Equal(42, list.Get(0));
+        }
+    }
+}
---
+++ ScratchPad/LinkedLists/Classes/LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+
+namespace LinkedLists.Classes
+{
+    public class LinkedList<T>
+    {
+        private Node<T> _head;
+        public int Count { get; set; }
+
+        public LinkedList()
+        {
+            Count = 0;
+            _head = null;
+        }
+
+        public void Insert(T data)
+        {
+            var node = CreateNode(data);
+            node.Next = _head;
+            _head = node;
+        }
+
+        private Node<T> CreateNode(T data)
+        {
+            Count++;
+            return new Node<T>
+            {
+                Data = data
+            };
+        }
+
+        public T Get(int index)
+        {
+            var position = 0;
+            Node<T> node = _head;
+            while (position < index && node != null)
+            {
+                node = node.Next;
+                position++;
+            }
+
+            return node != null ? node.Data : default;
+        }
+        private class Node<TData>
+        {
+            public TData Data;
+            public Node<TData> Next;
+        }
+    }
+
+}
