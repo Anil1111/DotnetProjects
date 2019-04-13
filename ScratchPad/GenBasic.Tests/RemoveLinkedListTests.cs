80d0f21f110960d4f90a7e965f8b497939ea9495
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-12 21:06:12 -0400

    added fluent assertions

---
+++ ScratchPad/GenBasic.Tests/RemoveLinkedListTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using FluentAssertions;
+using LinkedLists.Classes;
+using Xunit;
+
+namespace Generics.Tests
+{
+    public class RemoveLinkedListTests
+    {
+        [Fact]
+        public void RemoveWhere_RemoveLessThanFour_LessThanFourRemoved()
+        {
+            var list = new RemoveLinkedList<int>(1, 2, 3, 4, 5, 6);
+
+            list.Should().NotBeEmpty().And.HaveCount(6);
+            list.RemoveWhere(val => val < 4);
+            list.Should().NotBeEmpty().And.HaveCount(3);
+            list.Should().BeEquivalentTo(4, 5, 6);
+        }
+
+        [Fact]
+        public void RemoveWhere_GreaterThanFour_ReturnEquivalentValues()
+        {
+            var list = new RemoveLinkedList<int>(1,2,3,4,5,6);
+            list.Should().NotBeEmpty().And.HaveCount(6);
+
+            list.RemoveWhere(val => val > 4);
+            list.Should().NotBeEmpty().And.HaveCount(4);
+            list.Should().BeEquivalentTo(1, 2, 3, 4);
+        }
+
+    }
+}
---
+++ ScratchPad/LinkedLists/Classes/RemoveLinkedList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+
+namespace LinkedLists.Classes
+{
+    public class RemoveLinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> _head;
+
+        public int Count { get; set; }
+
+        public RemoveLinkedList()
+        {
+            Count = 0;
+            _head = null;
+        }
+
+        public RemoveLinkedList(params T[] values) : this()
+        {
+            foreach (var value in values)
+            {
+                Insert(value);
+            }
+        }
+
+        public void Insert(T data)
+        {
+            var node = CreateNode(data);
+            node.Next = _head;
+            _head = node;
+        }
+
+        private Node<T> CreateNode(T data)
+        {
+            Count++;
+            return new Node<T>
+            {
+                Data = data
+            };
+        }
+
+        public T Get(int index)
+        {
+            var position = 0;
+            Node<T> node = _head;
+            while (position < index && node != null)
+            {
+                node = node.Next;
+                position++;
+            }
+
+            if (node != null)
+            {
+                return node.Data;
+            }
+
+            return default;
+        }
+
+        private class Node<TData>
+        {
+            public TData Data;
+            public Node<TData> Next;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = _head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void RemoveWhere(Func<T, bool> condition)
+        {
+            // remove all nodes at the beginning of the list meeting the condition
+            while (_head != null && condition(_head.Data))
+            {
+                _head = _head.Next;
+            }
+
+            Node<T> node = _head;
+            while (node != null)
+            {
+                // while the next node meets the criteria, forget it
+                while (node.Next != null && condition(node.Next.Data))
+                {
+                    node.Next = node.Next.Next;
+                }
+
+                node = node.Next;
+            }
+        }
+    }
+
+}
+
