e361dd0f9965ed691ab68bf46e2775a856c3916c
FocusedBit <anthonyrayhollis@gmail.com>
2019-04-15 15:41:14 -0400

    added Frameworks

---
+++ ScratchPad/FrameWork/Classes/People.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FrameWork.Classes
+{
+    public class People : IEnumerable<Person>
+    {
+        private readonly Person[] _people;
+
+        public People(params Person[] people)
+        {
+            _people = people;
+        }
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+           return ((IEnumerable<Person>) _people).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ScratchPad/FrameWork/Classes/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace FrameWork.Classes
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
